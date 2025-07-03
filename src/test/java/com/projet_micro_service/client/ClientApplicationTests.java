f0e720f3b560ef8e9ff3d4bf2f08354d91366000
LouafiRazine <louafi.razine@gmail.com>
2025-07-03 15:14:01 +0200

    added project commit

---
+++ src/test/java/com/projet_micro_service/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_micro_service.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
